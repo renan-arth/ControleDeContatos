8c1233767689a120a6574cd32864c2ddeea3d2cb
Renan Arthur <renan.albino@fitbank.com.br>
2022-12-30 15:50:38 -0300

    Criando funções: Criar | Editar | Apagar

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -27,6 +27,12 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
+        public IActionResult Editar(int id)
+        {
+            UserModel usuario = _userRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
         [HttpPost]
         public IActionResult Criar(UserModel usuario)
         {
@@ -47,6 +53,67 @@ namespace ControleDeContatos.Controllers
                 return RedirectToAction("Index");
             }
 
+        }
+
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            UserModel usuario = _userRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _userRepositorio.Apagar(id);
+
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Usuário apagado com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, não conseguimos apagar o usuário. Tente novamente!";
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar o usuário, tente novamente, detalhe do erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Alterar(UserSemSenhaModel usuarioSemSenha)
+        {
+            try
+            {
+                UserModel usuario = null;
+
+                if (ModelState.IsValid)
+                {
+                    usuario = new UserModel()
+                    {
+                        Id = usuarioSemSenha.Id,
+                        Nome = usuarioSemSenha.Nome,
+                        Login = usuarioSemSenha.Login,
+                        Email = usuarioSemSenha.Email,
+                        Perfil = usuarioSemSenha.Perfil
+                    };
+
+                    usuario = _userRepositorio.Atualizar(usuario);
+                    TempData["MensagemSucesso"] = "Contato atualizado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View("Editar", usuario);
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos altualizar seu contato, tente novamente, detalhe do erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
