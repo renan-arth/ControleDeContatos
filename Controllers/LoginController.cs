f061b22333cf32a97cf4bdfcf63029d3cf02f34c
Renan Arthur <renan.albino@fitbank.com.br>
2023-01-02 15:35:05 -0300

    Tela de Login - Controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,31 @@ namespace ControleDeContatos.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (loginModel.Login == "Admin" &&  loginModel.Password == "123456")
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    TempData["MensagemErro"] = $"Usuário e/ou senha inválido(s). Por favor tente novamente";
+                    
+                }
+
+                return View("Index");
+            }
+            catch(Exception ex)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente, detalhe do erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
